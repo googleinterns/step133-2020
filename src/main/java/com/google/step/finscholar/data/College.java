bd71daf03eb05c81f1447b948c08e0279ce85d0a
MikolJiron <hurmi2156@outlook.com>
2020-06-29 17:09:46 +0000

    Change package name in College.java and delete GeneralInformation.java

--- src/main/java/com/google/step/finscholar/data/College.java
+++ src/main/java/com/google/step/finscholar/data/College.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.step.finscholar.data_objects;
+package com.google.step.finscholar.data;
 
 /** An object representing a College. */
 public final class College {
