c34bb7bbe6a9e00c2d6aa11598328cdda54b14ec
MikolJiron <hurmi2156@gmail.com>
2020-07-01 01:47:58 +0000

    Add separate class for JsonConverter

--- src/main/java/com/google/step/finscholar/servlets/CollegeServlet.java
+++ src/main/java/com/google/step/finscholar/servlets/CollegeServlet.java
@@ -17,6 +17,7 @@ package com.google.step.finscholar.servlets;
 import com.google.gson.Gson;
 import com.google.step.finscholar.data.College;
 import com.google.step.finscholar.data.CollegeData;
+import com.google.step.finscholar.servlets.JsonConverter;
 import java.io.IOException;
 import java.util.List;
 import javax.servlet.annotation.WebServlet;
@@ -35,21 +36,10 @@ public class CollegeServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Convert list of colleges to JSON.
     List<College> listOfColleges = CollegeData.COLLEGES;
-    String json = convertToJson(listOfColleges);
+    String json = JsonConverter.convertToJson(listOfColleges);
     
     // Send the list of colleges as the response.
     response.setContentType(JSON_CONTENT_TYPE);
     response.getWriter().println(json);
-  }
-
-  /**
-   * Converts an List<College> to a json string.
-   * @param listOfColleges - List<College> that needs to be converted.
-   * @return - Newly converted JSON string.
-   */
-  private String convertToJson(List<College> listOfColleges) {
-    Gson gson = new Gson();
-    String json = gson.toJson(listOfColleges);
-    return json;
   }
 }
