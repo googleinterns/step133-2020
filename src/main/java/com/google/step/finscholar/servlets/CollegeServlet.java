842c2c73bed7b1be4c7674ea432ba9ace80aabda
MikolJiron <hurmi2156@gmail.com>
2020-07-01 00:50:32 +0000

    Begin servlet for College

---
+++ src/main/java/com/google/step/finscholar/servlets/CollegeServlet.java
@@ -0,0 +1,57 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.step.finscholar.servlets;
+
+import com.google.gson.Gson;
+import com.google.step.finscholar.data.College;
+import com.google.step.finscholar.data.CollegeData;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that returns colleges and their associated data. */
+@WebServlet("/college-data")
+public class CollegeServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    
+    // Redirect back to the HTML page.
+    response.sendRedirect(Params.INDEX_PATH);
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  
+    // Send the list of comments as the response.
+    response.setContentType(Params.JSON_CONTENT_TYPE);
+    response.getWriter().println(json);
+  }
+
+  /**
+   * Converts an List<College> to a json string.
+   * @param listOfColleges - List<College> that needs to be converted.
+   * @return - Newly converted JSON string.
+   */
+  private String convertToJson(List<College> listOfColleges) {
+    Gson gson = new Gson();
+    String json = gson.toJson(listOfColleges);
+    return json;
+  }
+}
