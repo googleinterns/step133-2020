a85b8c4f44c0978bc0e9c05a92a75f14b415a2dd
Lorraine-Lyu <yl191@rice.edu>
2020-07-01 21:19:01 -0500

    finished basic scholarship template

--- src/main/webapp/templates/scholarshipPage/scholarshipPage.js
+++ src/main/webapp/templates/scholarshipPage/scholarshipPage.js
@@ -1,6 +1,48 @@
-restructureScholarshipObject(flatScholarship) => {
-  const introduction = {
-    flatScholarship.scholarshipName;
-    
+const restructureScholarshipObject = (flatScholarship) => {
+  return {
+    'introduction' : wrapIntro(flatScholarship),
+    'requirements' : removeInvalidFields(wrapRequirements(flatScholarship)),
+    'applicationNote' : removeinvalidFields(wrapApplicationNote(flatScholarship)),
+  };
+};
+
+const removeInvalidFields = (obj) => {
+  for (key in obj) {
+    if (obj[key] === [] || obj[key] === {value: null}) {
+      delete obj.key;
+    }
   }
 }
+
+const wrapIntro = ({scholarshipName, scholarshipUUID, introduction, URL}) => 
+                      ({scholarshipName, scholarshipUUID, introduction, URL,});
+
+const wrapRequirements = ({academicReuirements, 
+                           ethnicityRaceRequirements, 
+                           financialRequirements, 
+                           genderRequirements,
+                           locationrequirements, 
+                           nationalOriginRequirements,
+                           otherRequirements}) => ({
+                             academicReuirements, 
+                             ethnicityRaceRequirements, 
+                             financialRequirements,
+                             genderRequirements, 
+                             locationrequirements, 
+                             nationalOriginRequirements,
+                             otherRequirements,
+                           });
+                           
+const wrapApplicationNote = ({isRenewable, applicationProcess, amountPerYear, numberOfYears}) =>
+                                ({amountPerYear,
+                                  applicationProcess,
+                                  isRenewable,
+                                  numberOfYears,
+                                });
+const test = {
+  scholarshipName : 'name',
+  scholarshipUUID : 'uuid',
+  scholarshipSchool : 'abc',
+}
+
+console.log(unwrapIntro(test));
