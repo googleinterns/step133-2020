a5935b57a2d63c81388ec8e397cbaa209998a076
Lorraine-Lyu <yl191@rice.edu>
2020-07-03 19:43:55 -0500

    adds enum

---
+++ src/main/java/com/google/step/finscholar/data/Gender.java
@@ -0,0 +1,36 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.step.finscholar.data;
+
+/** The enumerator for gender. */
+public enum Gender {
+  
+    MALE("male"),
+    FEMALE("female"),
+    TRANSGENDER("transgender"),
+    NON_BINARY("non-binary"),
+    GENDER_NEUTRAL("gender neutral");
+
+    private String gender;
+
+    private Gender(String gender) {
+      this.gender = gender;
+    }
+
+    public String getValue() {
+      return this.gender;
+    }
+
+}
