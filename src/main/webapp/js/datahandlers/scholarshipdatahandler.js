b4ece8818148773515ba0138c8b86e54743dc205
Lorraine-Lyu <yl191@rice.edu>
2020-07-07 16:54:58 +0000

    adds comments and logs

--- src/main/webapp/js/datahandlers/scholarshipdatahandler.js
+++ src/main/webapp/js/datahandlers/scholarshipdatahandler.js
@@ -16,41 +16,45 @@
 
 goog.module('datahandlers.scholarshipdatahandler');
 
+/** This class loads scholarship data from backend and format it for soy template. */
 class ScholarshipDataHandler {
 
+  /**
+   * @constructor
+   */
   constructor() {
     /** 
      * This constant is the endpoint to send a fetch request to. 
      * @private @const
      */
-    this.ENDPOINT_ = '/scholarship-data';
+    this.SCHOLARSHIP_ENDPOINT_ = '/scholarship-data';
   }
 
   /**
    * This method converts from scholarship JSON object to a JS object map, 
    *  which will be used to render the scholarship page soy template.
-   * @private
    * @param {*} data - The JSON object to be converted.
    * @return {Object} - The object map representing a scholarship's data.
+   * @private
    */
   async convertFromJsonToTemplate_(data) {
-    const separator = ', ';
+    const SEPARATOR = ', ';
     return {
       generalInfo: {
         scholarshipName: data['scholarshipName'], 
         scholarshipUUID: data['scholarshipUUID'], 
-        schoolsList: data['schoolsList'].join(separator),
+        schoolsList: data['schoolsList'].join(SEPARATOR),
         introduction: data['introduction']['value'] || 'N/A', 
         URL: data['URL'],
       },
       requirements: {
-        academicRequirements: data['academicRequirements'].join(separator) || 'N/A', 
-        ethnicityRaceRequirements: data['ethnicityRaceRequirements'].join(separator) || 'N/A', 
-        financialRequirements: data['financialRequirements'].join(separator) || 'N/A',
-        genderRequirements: data['genderRequirements'].join(separator) || 'N/A',
-        locationRequirements: data['locationRequirements'].join(separator) || 'N/A',
-        nationalOriginRequirements: data['nationalOriginRequirements'].join(separator) || 'N/A',
-        otherRequirements: data['otherRequirements'].join(separator) || 'N/A',
+        academicRequirements: data['academicRequirements'].join(SEPARATOR) || 'N/A', 
+        ethnicityRaceRequirements: data['ethnicityRaceRequirements'].join(SEPARATOR) || 'N/A', 
+        financialRequirements: data['financialRequirements'].join(SEPARATOR) || 'N/A',
+        genderRequirements: data['genderRequirements'].join(SEPARATOR) || 'N/A',
+        locationRequirements: data['locationRequirements'].join(SEPARATOR) || 'N/A',
+        nationalOriginRequirements: data['nationalOriginRequirements'].join(SEPARATOR) || 'N/A',
+        otherRequirements: data['otherRequirements'].join(SEPARATOR) || 'N/A',
       },
       applicationNotes: {
         amountPerYear: data['amountPerYear']['value'] || 'unknown',
@@ -71,32 +75,35 @@ class ScholarshipDataHandler {
     try {
       data = await this.fetchScholarshipJson(id);
     } catch (e) {
+      console.log(e);
       throw(e);
     }
     
     // If data is undefined, bring user to an error page.
-    // This case will be handled in ScholarshipPageView.
+    // This check will be done in ScholarshipPageView.
     // Later after we support querying by uuid, data will be a acholarship object.
     return this.convertFromJsonToTemplate_(data[id]);
   }
 
   /**
    * Fetch request to the data servlet and return the JSON response.
-   * @private
    * @param {string} id The uuid of the schedule.
    * @return {*} - The JSON response.
+   * @private
    */
   async fetchScholarshipJson(id) {
-    const response = await fetch(this.ENDPOINT_, {'id': id });
+    const response = await fetch(this.SCHOLARSHIP_ENDPOINT_, {'id': id });
     let data = undefined;
     if (response.ok) {
       try {
         data = await response.json();
         return data;
       } catch (e) {
+        console.log(e);
         throw new Error(`Failed to parse response from server: ${e}`);
       }
     } else {
+      console.log(e);
       throw new Error(`Failed to get response from server: 
           ${response.statusText}. Status: ${response.status}`);
     }
