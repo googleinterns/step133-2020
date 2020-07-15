d0a46b6a8d4ec512129318d6492601b83d156fc3
jmj51 <jmj51@duke.edu>
2020-07-14 20:04:59 -0400

    Fix college data

--- src/main/java/com/google/step/finscholar/data/CollegeData.java
+++ src/main/java/com/google/step/finscholar/data/CollegeData.java
@@ -27,11 +27,10 @@ public final class CollegeData {
     List<UUID> users = new ArrayList<UUID>();
     users.add(UUID.randomUUID());
     
-    College school = new College.CollegeBuilder("Duke University")
+    College school = new College.CollegeBuilder("Duke University", "ID")
       .setInstitutionType("Private")
       .setAcceptanceRate(0.07)
       .setAverageACTScore(33)
-      .setUsersUUIDList(users)
       .setTotalCostAttendance(75000)
       .setNetCostForFirstQuintile(2000)
       .setNetCostForSecondQuintile(5000)
