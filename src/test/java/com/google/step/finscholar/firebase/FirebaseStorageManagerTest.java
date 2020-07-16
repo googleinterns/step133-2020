d07a202a1058e1e266167d472ed2bd60258b16dd
jmj51 <jmj51@duke.edu>
2020-07-16 13:24:46 -0400

    Add firebase test class

---
+++ src/test/java/com/google/step/finscholar/firebase/FirebaseStorageManagerTest.java
@@ -0,0 +1,19 @@
+package com.google.step.finscholar.firebase;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FirebaseStorageManagerTest {
+  @BeforeClass
+  public static void setUp() {
+    
+  }
+  
+  @Test
+
+}
+
