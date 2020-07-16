dd03d98af19bfc5e24dbb26abb69c94a93625ac1
jmj51 <jmj51@duke.edu>
2020-07-16 17:26:00 -0400

    Add comments to TestObject

--- src/main/java/com/google/step/finscholar/data/TestObject.java
+++ src/main/java/com/google/step/finscholar/data/TestObject.java
@@ -15,32 +15,63 @@
 
 package com.google.step.finscholar.data;
 
+/** 
+ * This simple object is used in testing our Firebase methods' behaviour. 
+ * All objects stored in Firestore must be a POJO (Plain Old Java Object). This means they must have
+ *   private non-final instance variables. An empty no-parameter constructor,
+ *   a non-empty constructor, and setters and getters for each instance
+ *   variable. As long as an object is a POJO, the functionality applies to every
+ *   other POJO object the same way. This also applies to POJO objects that are instantiated 
+ *   using the builder pattern.
+ */
 public class TestObject {
 
   private String one;
   private String two;
 
-  public TestObject() {
+  public TestObject() {}
 
-  }
-
+  /**
+   * @param one - An arbitrary string for the first private instance variable.
+   * @param two - An arbitrary string for the second private instance variable.
+   */
   public TestObject(String one, String two) {
     this.one = one;
     this.two = two;
   }
 
-  public void setOne(String newOne) {
+  /**
+   * The required setter for "one" to make this object into a POJO.
+   * @param newOne
+   * @return - This object.
+   */
+  public TestObject setOne(String newOne) {
     this.one = newOne;
+    return this;
   }
 
-  public void setTwo(String newTwo) {
+  /**
+   * The required setter for "two" to make this object into a POJO.
+   * @param newOne
+   * @return This object.
+   */
+  public TestObject setTwo(String newTwo) {
     this.two = newTwo;
+    return this;
   }
 
+  /**
+   * The required getter for "one" to make this object into a POJO.
+   * @return - The value of the "one" private instance variable.
+   */
   public String getOne() {
     return this.one;
   }
 
+  /**
+   * The required getter for "two" to make this object into a POJO.
+   * @return - The value of the "two" private instance variable.
+   */
   public String getTwo() {
     return this.two;
   }
