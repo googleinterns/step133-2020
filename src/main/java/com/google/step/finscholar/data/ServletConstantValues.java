9afc8bc4c10206602fc7d39b6dfcda3ba98dc462
jmj51 <jmj51@duke.edu>
2020-07-20 18:56:19 -0400

    Restore firebase-setup to it's previous form

--- src/main/java/com/google/step/finscholar/data/ServletConstantValues.java
+++ src/main/java/com/google/step/finscholar/data/ServletConstantValues.java
@@ -19,6 +19,15 @@ public class ServletConstantValues {
     public static final String JSON_CONTENT_TYPE = "application/json";	
     public static final String INDEX_PATH = "/index.html";	
     // The default value for undefined fields.	
-    public static final String DEFAULT_VALUE = "";	
+    public static final String DEFAULT_VALUE = "";
+    
+    // Constants for exceptions when retrieving data from the Firestore database.
+    public static final String UNABLE_TO_LOAD_FIREBASE = "Unable to load firebase: ";
+
+    // Cosntants for FirebaseAppManager.
+    public static final String SCOPE_CLOUD_URL = "https://www.googleapis.com/auth/cloud-platform";
+    public static final String SCOPE_USER_INFO_URL = "https://www.googleapis.com/auth/userinfo.email";
+    public static final String DATABASE_URL = "https://viewing-step-2020-v2.firebaseio.com";
+    public static final String PROJECT_ID = "viewing-step-2020-v2";	
 
 } 
