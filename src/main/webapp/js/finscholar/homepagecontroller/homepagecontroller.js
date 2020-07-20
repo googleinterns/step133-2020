db32fc4d9e970e1f84bb4fef60dbb92bffa1a92b
jmj51 <jmj51@duke.edu>
2020-07-20 16:15:34 -0400

    fix merge conflict error

--- src/main/webapp/js/finscholar/homepagecontroller/homepagecontroller.js
+++ src/main/webapp/js/finscholar/homepagecontroller/homepagecontroller.js
@@ -16,7 +16,6 @@
 
 goog.module('finscholar.homepagecontroller');
 
-<<<<<<< HEAD
 const JsactionActionFlow = goog.require('jsaction.ActionFlow');
 const JsactionDispatcher = goog.require('jsaction.Dispatcher');
 const JsactionEventContract = goog.require('jsaction.EventContract');
@@ -27,11 +26,6 @@ const {homepage} = goog.require('finscholar.homepagecontroller.templates');
 const {CollegeListView} = goog.require('finscholar.collegelistview');
 const {CollegePageView} = goog.require('finscholar.collegepageview');
 const {ErrorData, ErrorPageView} = goog.require('finscholar.errorpageview');
-=======
-const {CollegeListView} = goog.require('finscholar.collegelistview');
-const {CollegePageView} = goog.require('finscholar.collegepageview');
-const {ErrorPageView} = goog.require('finscholar.errorpageview');
->>>>>>> devel
 const {PageController} = goog.require('pagecontroller');
 const {ScholarshipListView} = goog.require('finscholar.scholarshiplistview');
 const {ScholarshipPageView} = goog.require('finscholar.scholarshippageview');
