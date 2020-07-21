13c94df0e29a4b348545ce1c5ae48339ccc3d7ca
Carlos Medrano <crlsmedrano99@gmail.com>
2020-07-21 14:53:33 -0500

    Refactor collegelistview to use base super class

--- src/main/webapp/js/finscholar/collegelistview/collegelistview.js
+++ src/main/webapp/js/finscholar/collegelistview/collegelistview.js
@@ -15,21 +15,23 @@
 /** @fileoverview The mini controller for college list view. */
 
 goog.module('finscholar.collegelistview');
+const {BasicView} = goog.require('basicview');
 const {collegelist} = goog.require('finscholar.collegelistview.templates');
 
 /** The mini controller for college list view. */
-class CollegeListView {
-  
+class CollegeListView extends BasicView {
   constructor() {
+    super();
   }
 
   /**
    * Renders a college list view to the container.
-   * @param {!Element} container The HTML container to load the view.
+   * @override
    */
-  renderView(container) {
-    container.innerHTML = collegelist();
+  async renderView() {
+    super.setCurrentContent(collegelist());
+    super.resetAndUpdate();
   }
 }
 
-exports = {CollegeListView}
+exports = {CollegeListView};
