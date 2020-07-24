18e0e89c906c8a35bac0dc19376b62c75293f85d
jmj51 <jmj51@duke.edu>
2020-07-24 16:47:45 -0400

    Fix expected issue

--- src/test/java/com/google/step/finscholar/firebase/FirebaseStorageManagerTest.java
+++ src/test/java/com/google/step/finscholar/firebase/FirebaseStorageManagerTest.java
@@ -32,7 +32,7 @@ public class FirebaseStorageManagerTest {
   public static final String TEST_DOCUMENT_SEVEN_NAME = "testDocument7";
   public static final String TEST_DOCUMENT_EIGHT_NAME = "testDocument8";
   public static final String EXPECTED_DOCUMENT_RETRIEVABLE = "{\"one\":\"testObjects\",\"two\":\"testDocument\"}";
-  public static final String EXPECTED_COLLECTION_WITH_ID = "[{\"one\":\"testDocument\",\"two\":\"testObjects\"}]";
+public static final String EXPECTED_COLLECTION_WITH_ID = "[{\"one\":\"testDocument\",\"two\":\"testObjects\"}]";
   public static final String EXPECTED_COLLECTION_NO_ID = "";
   public static final int TEST_BATCH_SIZE_LIMIT = 10;
   public static final int TEST_BATCH_SIZE_LOWER = 3;
@@ -81,7 +81,7 @@ public class FirebaseStorageManagerTest {
   public void isRecentlySetDocumentRetrievable() throws Exception {
     FirebaseStorageManager.storeDocument(firebase, TEST_COLLECTION_NAME, testObject, TEST_DOCUMENT_NAME);
     String json = FirebaseStorageManager.getDocument(firebase, TEST_COLLECTION_NAME, TEST_DOCUMENT_NAME);
-    Assert.assertEquals(json, EXPECTED_DOCUMENT_RETRIEVABLE);
+    Assert.assertEquals(EXPECTED_DOCUMENT_RETRIEVABLE,json);
   }
 
   @Test
