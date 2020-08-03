34be7379d922966fba117d87aa141c5639f148ce
Lorraine-Lyu <yl191@rice.edu>
2020-08-03 16:56:12 +0000

    adds doc string

--- src/main/webapp/js/finscholar/commonlistview/commonlistview.js
+++ src/main/webapp/js/finscholar/commonlistview/commonlistview.js
@@ -207,9 +207,7 @@ class CommonListView extends BasicView {
     });
   }
 
-  /**
-   * @private Preset the hight of the list.
-   */
+  /** @private Preset the hight of the list. */
   setHeight_() {
     const listNodeHeight = 
         googDom.getFirstElementChild(this.container_).clientHeight;
